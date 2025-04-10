712731628bd2e812db565016eb61f5f6fc83df84
CYBER-YT <anilkumarmishraji52@gmail.com>
2025-04-10 17:49:55 +0530

    Update userbot.py

--- PROMUSIC/core/userbot.py
+++ PROMUSIC/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ProfessorXSupport")
-                await self.one.join_chat("ProfessorSourabh")
+                await self.one.join_chat("StylishNamesFont")
+                await self.one.join_chat("BOTXPRO1")
             except:
                 pass
             assistants.append(1)
